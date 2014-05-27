6078c4509fd730a8c89a429f0d083ee04dc0e72b
Stefan Eng <stefaneng13@gmail.com>
2014-05-26 19:13:23 -0700

    Makes 6 opaque circles with text

--- js/catalog.js
+++ js/catalog.js
@@ -1,3 +1,20 @@
 (function() {
-    console.log("Hello world");
+    "use strict";
+
+    var g = d3.select("svg")
+	    .append("g")
+              .attr("transform", "translate(90,90)");
+
+    var circles = g.selectAll("g")
+	    .data([1,2,3,4,5,6])
+	    .enter().append("g")
+	    .attr("transform" , function(d) { return "translate(" + d * 60 + ",0)"});
+
+    circles.append("circle")
+	.attr("r", 30);
+
+    circles.append("text")
+	.attr("text-anchor", "middle")
+	.attr("dy", "5px")
+	.text(function(d) { return d; });
 })();
