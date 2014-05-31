6fceadca3e2c25b0e07b8e54aa3788860e79ae38
Stefan Eng <stefaneng13@gmail.com>
2014-05-30 22:10:52 -0700

    Initial force layout

--- js/catalog.js
+++ js/catalog.js
@@ -1,50 +1,31 @@
 (function() {
     "use strict";
 
+    var width = 960,
+	height = 500;
+
     var g = d3.select("svg")
+	    .attr("width", width)
+	    .attr("height", height)
 	    .append("g")
               .attr("transform", "translate(90,90)");
 
+    var force = d3.layout.force()
+	    .charge(-240)
+	    .size([width, height]);
+
     d3.json("/data/sample_courses.json", function(error, courses) {
-	var circles = g.selectAll("g")
+	force.nodes(courses)
+	    .start();
+
+	var node = g.selectAll("circle")
 		.data(courses)
-		.enter().append("g")
-		.attr("transform", "translate(20,0)");
-
-	var circleMouseover = function(d) {
-	    d3.select("body")
-		.append("div")
-		.attr("class", "tooltip")
-		.text(d.description);
-
-	    d3.select(this)
-		.transition()
-		.style("opacity", 0.7);
-	};
-
-	var circleMousemove = function() {
-	    d3.select(".tooltip")
-		.style("left", (d3.event.pageX + 5) + "px")
-		.style("top", (d3.event.pageY - 35) + "px");
-	};
-
-	var circleMouseout = function() {
-	    d3.select(".tooltip")
-		.remove();
-	    d3.select(this)
-		.transition()
-		.style("opacity", 0.5);
-	};
-
-	circles.append("circle")
-	    .attr("r", 35)
-	    .on("mouseover", circleMouseover)
-	    .on("mousemove", circleMousemove)
-	    .on("mouseout", circleMouseout);
-
-	circles.append("text")
-	    .attr("text-anchor", "middle")
-	    .attr("dy", "5px")
-	    .text(function(d) { return d.classname; });
+		.enter().append("circle")
+		.attr("r", 25)
+		.call(force.drag);
+	force.on("tick", function() {
+	    node.attr("cx", function(d) { return d.x; })
+		.attr("cy", function(d) { return d.y; });
+	});
     });
 })();
