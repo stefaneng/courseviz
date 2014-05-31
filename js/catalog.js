31783b38e47d624270ce5a5a0c9852fb92dcb53b
Stefan Eng <stefaneng13@gmail.com>
2014-05-30 19:20:33 -0700

    Display one course with mouseover description

--- js/catalog.js
+++ js/catalog.js
@@ -1,52 +1,50 @@
 (function() {
     "use strict";
 
-    var data = [1,2,3,4];
-
     var g = d3.select("svg")
 	    .append("g")
               .attr("transform", "translate(90,90)");
 
-    var circles = g.selectAll("g")
-	    .data(data)
-	    .enter().append("g")
-	    .attr("transform" , function(d) { return "translate(" + d * 60 + ",0)"});
+    d3.json("/data/sample_courses.json", function(error, courses) {
+	var circles = g.selectAll("g")
+		.data(courses)
+		.enter().append("g")
+		.attr("transform", "translate(20,0)");
 
+	var circleMouseover = function(d) {
+	    d3.select("body")
+		.append("div")
+		.attr("class", "tooltip")
+		.text(d.description);
 
-    var circleMouseover = function(d) {
-	d3.select("body")
-	    .append("div")
-	    .attr("class", "tooltip")
-	    .text("Item number: " + d);
+	    d3.select(this)
+		.transition()
+		.style("opacity", 0.7);
+	};
 
-	d3.select(this)
-	    .transition()
-	    .style("opacity", 0.7);
-    };
+	var circleMousemove = function() {
+	    d3.select(".tooltip")
+		.style("left", (d3.event.pageX + 5) + "px")
+		.style("top", (d3.event.pageY - 35) + "px");
+	};
 
-    var circleMousemove = function() {
-	d3.select(".tooltip")
-	    .style("left", (d3.event.pageX + 5) + "px")
-	    .style("top", (d3.event.pageY - 35) + "px");
-    };
+	var circleMouseout = function() {
+	    d3.select(".tooltip")
+		.remove();
+	    d3.select(this)
+		.transition()
+		.style("opacity", 0.5);
+	};
 
-    var circleMouseout = function() {
-	d3.select(".tooltip")
-	    .remove();
-	d3.select(this)
-	    .transition()
-	    .style("opacity", 0.5);
-    };
+	circles.append("circle")
+	    .attr("r", 35)
+	    .on("mouseover", circleMouseover)
+	    .on("mousemove", circleMousemove)
+	    .on("mouseout", circleMouseout);
 
-    circles.append("circle")
-	.attr("r", 30)
-	.on("mouseover", circleMouseover)
-	.on("mousemove", circleMousemove)
-	.on("mouseout", circleMouseout);
-
-    circles.append("text")
-	.attr("text-anchor", "middle")
-	.attr("dy", "5px")
-	.text(function(d) { return d; });
-
+	circles.append("text")
+	    .attr("text-anchor", "middle")
+	    .attr("dy", "5px")
+	    .text(function(d) { return d.classname; });
+    });
 })();
